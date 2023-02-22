788eb1ccf64ceb429f5429b36c4649b2351d4565
Antonio Israel Cruz Hernandez <119275593+Isracx92@users.noreply.github.com>
2023-02-22 01:15:49 -0600

    Add files via upload

---
+++ Artista/src/mx/album/dto/Grupo.java
@@ -0,0 +1,36 @@
+package mx.album.dto;
+
+import java.util.List;
+
+public class Grupo extends Artista{
+
+	private int id;
+	private String nombreGrupo;
+	private List<Artista> Artista;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombreGrupo() {
+		return nombreGrupo;
+	}
+	public void setNombreGrupo(String nombreGrupo) {
+		this.nombreGrupo = nombreGrupo;
+	}
+	public List<Artista> getArtista() {
+		return Artista;
+	}
+	public void setArtista(List<Artista> artista) {
+		Artista = artista;
+	}
+	@Override
+	public String toString() {
+		return "Grupo [id=" + id + ", nombreGrupo=" + nombreGrupo + ", Artista=" + Artista + "]";
+	}
+	
+	
+}
